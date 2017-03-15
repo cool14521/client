e71c6ef393786c67d1cf858ea3803da9f13beacb
Mike Maxim <mike@keyba.se>
2017-03-15 11:17:02 -0400

    wip

--- shared/constants/types/flow-types-chat.js
+++ shared/constants/types/flow-types-chat.js
@@ -906,7 +906,7 @@ export type GetInboxByTLFIDRemoteRes = {
 export type GetInboxLocalQuery = {
   tlfName?: ?string,
   topicName?: ?string,
-  convID?: ?ConversationID,
+  convIDs?: ?Array<ConversationID>,
   topicType?: ?TopicType,
   tlfVisibility?: ?TLFVisibility,
   before?: ?gregor1.Time,
@@ -1599,6 +1599,7 @@ export type localGetInboxAndUnboxLocalRpcParam = Exact<{
 }>
 
 export type localGetInboxNonblockLocalRpcParam = Exact<{
+  maxUnbox?: ?int,
   query?: ?GetInboxLocalQuery,
   pagination?: ?Pagination,
   identifyBehavior: keybase1.TLFIdentifyBehavior
